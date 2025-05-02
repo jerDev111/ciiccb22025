140aaa0177732666c5e0a470a87e070379df213f
Jerwyn Ponio <jerwyn.dev@gmail.com>
2025-05-02 07:27:34 +0000

    new file:   Task2.java 	new file:   Task3.java

---
+++ Task2.java
@@ -0,0 +1,21 @@
+public class Task2 {
+    public static void main(String[] ciiccb2) {
+    
+        char h = 'H';
+        short s = 311;
+        int i = 0;
+        char w = 'w';
+        byte b = 0;
+        char r = 'r';
+        int one = 1;
+        char d = 'd';
+        float f = 2.0f;
+        boolean tama = true;
+
+
+ String output = ""+ h + s + i + " " + w + b + r + one + d + " " + f + " " + tama;
+ 
+ 
+        System.out.println(output);
+    }
+}
