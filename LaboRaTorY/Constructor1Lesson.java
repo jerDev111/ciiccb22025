ed39520c1d76bb94c53eb3af2fc63cd782cacc4b
Jerwyn Ponio <jerwyn.dev@gmail.com>
2025-06-05 06:41:31 +0000

    new file:   LaboRaTorY/Constructor1Lesson.java

---
+++ LaboRaTorY/Constructor1Lesson.java
@@ -0,0 +1,15 @@
+package LaboRaTorY;
+class Chicken{
+    int numEggs = 0;
+    String name = "duke";
+}
+
+public class Constructor1Lesson {
+    
+    public static void main(String[] args) {
+        Chicken c1 = new Chicken();
+        System.out.println(c1.name);
+    }
+
+    
+}
